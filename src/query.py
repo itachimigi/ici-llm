b6550e985cd03078aa7dd11e691e126b9c231cb0
itachimigi <101614099+itachimigi@users.noreply.github.com>
2025-06-16 12:57:53 +0800

    Update query.py

--- src/query.py
+++ src/query.py
@@ -1,13 +1,14 @@
 import os, html
 import argparse
 import time
+import json
 import pandas as pd
 from pubmed_parser import parse_pubmed_xml, parse_pubmed_paragraph, parse_pubmed_caption
-
+from openai import OpenAI
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils.zeroshot import get_response as get_zero_response
-from utils.fewshot import get_response as get_few_response
+from utils.zeroshot import get_zeroshot_input
+from utils.fewshot import get_fewshot_input
 
 
 def parse_args():
@@ -16,9 +17,9 @@ def parse_args():
 
     p.add_argument('--xml-dir', required=True, help="Cleaned XML input directory")
     p.add_argument('--out-dir', required=True, help="Output directory for responses")
-    p.add_argument('--model', required=True, help="OpenAI model name")
-    p.add_argument('--api-key', required=True, help="OpenAI API key")
-    p.add_argument('--mode', choices=['zero','few'], default='zero', help="zero-shot or few-shot")
+    p.add_argument('--model', required=True, help="Model name: gpt-3.5-turbo, gpt-4-turbo, gpt-4o, deepseek-r1, gemini-2.5")
+    p.add_argument('--api-key', required=True, help="API key")
+
     args = p.parse_args()
 
     return args
@@ -36,31 +37,130 @@ def get_text(path):
             text += content + '\n'
     return html.unescape(text)
 
+def process_response(response, df, index, columns):
+    """Process API response and update DataFrame."""
+    try:
+        result = json.loads(response)
+        df.loc[index,columns] = result
 
-def main(xml_dir, out_dir, model, api_key, mode):
+    except:
+        df.loc[index, columns[0]] = response
+
+def call_LLM(api_key, model, system_message, user_message):
+
+    if model == "gpt-3.5-turbo":
+        model = "gpt-3.5-turbo-0125"
+        base_url = "https://api.openai.com/v1"
+    elif model == "gpt-4-turbo":
+        model = "gpt-4-turbo-2024-04-09"
+        base_url = "https://api.openai.com/v1"
+    elif model == "gpt-4o":
+        model = "gpt-4o-2024-05-13"
+        base_url = "https://api.openai.com/v1"
+    elif model == "deepseek-r1":
+        model = "deepseek-reasoner"
+        base_url = "https://api.deepseek.com"
+    elif model == "gemini-2.5":
+        model = "gemini-2.5-flash-preview-05-20"
+        base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
+
+
+    client = OpenAI(
+        api_key=api_key,
+        base_url=base_url
+    )
+    
+    params = {
+        "model": model,
+        "temperature": 0.1,
+        "response_format": { "type": "json_object" },
+        "messages": [
+            {
+                "role": "system",
+                "content": system_message,
+            },
+            {
+                "role": "user",
+                "content": user_message,
+            },
+        ]
+    }
+
+    if model.startswith("gemini"):
+        params["reasoning_effort"] = "high"
+
+    completion = client.chat.completions.create(**params)
+
+    response = completion.choices[0].message.content
+
+    return response
+
+def main(xml_dir, out_dir, model, api_key):
+
     os.makedirs(out_dir, exist_ok=True)
-    os.environ['api_key'] = api_key
+
     files = [f for f in os.listdir(xml_dir) if f.lower().endswith(('.xml', '.nxml'))]
-    df = pd.DataFrame(columns=['id', 'definition', 'others'])
+    cols = ['id', 'Predictor', 'Feature', 'Model',
+        'Data Required', 'Biosample Required', 'Cancer', 'Species', 'Treatment',
+        'Cohort Details', 'Target Outcome', 'Correlation with Efficacy']
+    df_zeroshot = pd.DataFrame(columns=cols, index=range(len(files)))
+    
+    df_fewshot = df_zeroshot.copy()
 
     for i, fn in enumerate(files):
         paper_id = os.path.splitext(fn)[0]
         path = os.path.join(xml_dir, fn)
         text = get_text(path)
-        if mode == 'zero':
-            resp1 = get_zero_response(api_key, model, True, text)
-            time.sleep(2)
-            resp2 = get_zero_response(api_key, model, False, text)
-        else:
-            resp1 = get_few_response(api_key, model, True, text)
-            time.sleep(2)
-            resp2 = get_few_response(api_key, model, False, text)
-        df.loc[i] = [paper_id, resp1, resp2]
 
-    out_path = os.path.join(out_dir, f"responses_{model.replace('/', '_')}.xlsx")
-    df.to_excel(out_path, index=False)
+        # Prepare system and user messages for zeroshot  prompts  
+        system_message_zeroshot_1, user_message_zeroshot_1 = get_zeroshot_input(True, text)
+        system_message_zeroshot_2, user_message_zeroshot_2 = get_zeroshot_input(False, text)
+
+        # Prepare system and user messages for fewshot prompts
+        system_message_fewshot_1, user_message_fewshot_1 = get_fewshot_input(True, text)
+        system_message_fewshot_2, user_message_fewshot_2 = get_fewshot_input(False, text)
+
+        try:
+            response_zeroshot_1 = call_LLM(api_key,  model, system_message_zeroshot_1, user_message_zeroshot_1)
+        except:
+            print(f"Error calling LLM for {paper_id} zeroshot_1")
+            response_zeroshot_1 = "Error"
+
+        time.sleep(2)
+        try:
+            response_zeroshot_2 = call_LLM(api_key, model, system_message_zeroshot_2, user_message_zeroshot_2)
+        except:
+            print(f"Error calling LLM for {paper_id} zeroshot_2")
+            response_zeroshot_2 = "Error"
+        
+        time.sleep(2)
+        try:
+            response_fewshot_1 = call_LLM(api_key, model, system_message_fewshot_1, user_message_fewshot_1)
+        except:
+            print(f"Error calling LLM for {paper_id} fewshot_1")
+            response_fewshot_1 = "Error"
+        
+        time.sleep(2)
+        try:
+            response_fewshot_2 = call_LLM(api_key, model, system_message_fewshot_2, user_message_fewshot_2)
+        except:
+            print(f"Error calling LLM for {paper_id} fewshot_2")
+            response_fewshot_2 = "Error"
+
+        df_zeroshot.loc[i, 'id'] = paper_id
+        df_fewshot.loc[i, 'id'] = paper_id
+
+        process_response(response_zeroshot_1, df_zeroshot, i, cols[1:4])
+        process_response(response_zeroshot_2, df_zeroshot, i, cols[4:])
+        process_response(response_fewshot_1, df_fewshot, i, cols[1:4])
+        process_response(response_fewshot_2, df_fewshot, i, cols[4:])
+
+
+    df_zeroshot.to_excel(os.path.join(out_dir, f"responses_{model}_zeroshot.xlsx"), index=False)
+    df_fewshot.to_excel(os.path.join(out_dir, f"responses_{model}_fewshot.xlsx"), index=False)
+
 
 if __name__ == '__main__':
 
     args = parse_args()
-    main(args.xml_dir, args.out_dir, args.model, args.api_key, args.mode)
+    main(args.xml_dir, args.out_dir, args.model, args.api_key)
